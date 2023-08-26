d4f4893c0251de677b9758353794a1c5ef386a4d
JD <jondavidperson@gmail.com>
2023-08-26 14:14:00 -0500

    setup Thought model and Reaction schema

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
