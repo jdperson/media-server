aac73147b544d5a33fc5aaeda024c2ee72d27c61
JD <jondavidperson@gmail.com>
2023-08-26 15:30:14 -0500

    moved mongo connection link to .env

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb+srv://root:root@cluster0.v4nwp1a.mongodb.net/devApps');
+connect(process.env.MONGO_CONN);
 
 module.exports = connection;
